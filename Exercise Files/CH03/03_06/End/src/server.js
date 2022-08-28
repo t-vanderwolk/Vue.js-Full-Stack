a5dc862dfec17043d2f1c36bb9100a6158144fd3
Taylor Vanderwolk <taylorvanderwolk@Taylors-MacBook-Pro.local>
2022-08-28 09:38:16 -0700

    Excercise files

---
+++ Exercise Files/CH03/03_06/End/src/server.js
@@ -0,0 +1,188 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import { MongoClient } from 'mongodb';
+
+const products = [{
+  id: '123',
+  name: 'Running Shoes',
+  price: '60.00',
+  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel enim quam. Mauris nisl tellus, fringilla sed cursus eu, convallis non diam. Mauris quis fringilla nunc. Aenean leo lacus, lobortis sit amet venenatis a, aliquet tristique erat. Etiam laoreet mauris ut dapibus tincidunt. Pellentesque non ex at nisl ornare aliquam sed non ante. Nam lobortis magna id massa cursus, sit amet condimentum metus facilisis. Donec eu tortor at est tempor cursus et sed velit. Morbi rutrum elementum est vitae fringilla. Phasellus dignissim purus turpis, ac varius enim auctor vulputate. In ullamcorper vestibulum mauris. Nulla malesuada pretium mauris, lobortis eleifend dolor iaculis vitae.',
+  imageUrl: '/images/shoes-1.jpg',
+  averageRating: '5.0',
+}, {
+  id: '234',
+  name: 'Basketball Shoes',
+  price: '120.00',
+  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel enim quam. Mauris nisl tellus, fringilla sed cursus eu, convallis non diam. Mauris quis fringilla nunc. Aenean leo lacus, lobortis sit amet venenatis a, aliquet tristique erat. Etiam laoreet mauris ut dapibus tincidunt. Pellentesque non ex at nisl ornare aliquam sed non ante. Nam lobortis magna id massa cursus, sit amet condimentum metus facilisis. Donec eu tortor at est tempor cursus et sed velit. Morbi rutrum elementum est vitae fringilla. Phasellus dignissim purus turpis, ac varius enim auctor vulputate. In ullamcorper vestibulum mauris. Nulla malesuada pretium mauris, lobortis eleifend dolor iaculis vitae.',
+  imageUrl: '/images/shoes-2.jpg',
+  averageRating: '5.0',
+}, {
+  id: '345',
+  name: 'Bright Red Shoes',
+  price: '90.00',
+  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel enim quam. Mauris nisl tellus, fringilla sed cursus eu, convallis non diam. Mauris quis fringilla nunc. Aenean leo lacus, lobortis sit amet venenatis a, aliquet tristique erat. Etiam laoreet mauris ut dapibus tincidunt. Pellentesque non ex at nisl ornare aliquam sed non ante. Nam lobortis magna id massa cursus, sit amet condimentum metus facilisis. Donec eu tortor at est tempor cursus et sed velit. Morbi rutrum elementum est vitae fringilla. Phasellus dignissim purus turpis, ac varius enim auctor vulputate. In ullamcorper vestibulum mauris. Nulla malesuada pretium mauris, lobortis eleifend dolor iaculis vitae.',
+  imageUrl: '/images/shoes-3.jpg',
+  averageRating: '5.0',
+}, {
+  id: '456',
+  name: 'Fancy Shoes',
+  price: '190.00',
+  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel enim quam. Mauris nisl tellus, fringilla sed cursus eu, convallis non diam. Mauris quis fringilla nunc. Aenean leo lacus, lobortis sit amet venenatis a, aliquet tristique erat. Etiam laoreet mauris ut dapibus tincidunt. Pellentesque non ex at nisl ornare aliquam sed non ante. Nam lobortis magna id massa cursus, sit amet condimentum metus facilisis. Donec eu tortor at est tempor cursus et sed velit. Morbi rutrum elementum est vitae fringilla. Phasellus dignissim purus turpis, ac varius enim auctor vulputate. In ullamcorper vestibulum mauris. Nulla malesuada pretium mauris, lobortis eleifend dolor iaculis vitae.',
+  imageUrl: '/images/shoes-4.jpg',
+  averageRating: '5.0',
+}, {
+  id: '567',
+  name: 'Skateboard Shoes',
+  price: '75.00',
+  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel enim quam. Mauris nisl tellus, fringilla sed cursus eu, convallis non diam. Mauris quis fringilla nunc. Aenean leo lacus, lobortis sit amet venenatis a, aliquet tristique erat. Etiam laoreet mauris ut dapibus tincidunt. Pellentesque non ex at nisl ornare aliquam sed non ante. Nam lobortis magna id massa cursus, sit amet condimentum metus facilisis. Donec eu tortor at est tempor cursus et sed velit. Morbi rutrum elementum est vitae fringilla. Phasellus dignissim purus turpis, ac varius enim auctor vulputate. In ullamcorper vestibulum mauris. Nulla malesuada pretium mauris, lobortis eleifend dolor iaculis vitae.',
+  imageUrl: '/images/shoes-5.jpg',
+  averageRating: '5.0',
+}, {
+  id: '678',
+  name: 'High Heels',
+  price: '200.00',
+  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel enim quam. Mauris nisl tellus, fringilla sed cursus eu, convallis non diam. Mauris quis fringilla nunc. Aenean leo lacus, lobortis sit amet venenatis a, aliquet tristique erat. Etiam laoreet mauris ut dapibus tincidunt. Pellentesque non ex at nisl ornare aliquam sed non ante. Nam lobortis magna id massa cursus, sit amet condimentum metus facilisis. Donec eu tortor at est tempor cursus et sed velit. Morbi rutrum elementum est vitae fringilla. Phasellus dignissim purus turpis, ac varius enim auctor vulputate. In ullamcorper vestibulum mauris. Nulla malesuada pretium mauris, lobortis eleifend dolor iaculis vitae.',
+  imageUrl: '/images/shoes-6.jpg',
+  averageRating: '5.0',
+}, {
+  id: '789',
+  name: 'Dark Shoes',
+  price: '100.00',
+  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel enim quam. Mauris nisl tellus, fringilla sed cursus eu, convallis non diam. Mauris quis fringilla nunc. Aenean leo lacus, lobortis sit amet venenatis a, aliquet tristique erat. Etiam laoreet mauris ut dapibus tincidunt. Pellentesque non ex at nisl ornare aliquam sed non ante. Nam lobortis magna id massa cursus, sit amet condimentum metus facilisis. Donec eu tortor at est tempor cursus et sed velit. Morbi rutrum elementum est vitae fringilla. Phasellus dignissim purus turpis, ac varius enim auctor vulputate. In ullamcorper vestibulum mauris. Nulla malesuada pretium mauris, lobortis eleifend dolor iaculis vitae.',
+  imageUrl: '/images/shoes-7.jpg',
+  averageRating: '5.0',
+}, {
+  id: '890',
+  name: 'Classic Shoes',
+  price: '40.00',
+  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel enim quam. Mauris nisl tellus, fringilla sed cursus eu, convallis non diam. Mauris quis fringilla nunc. Aenean leo lacus, lobortis sit amet venenatis a, aliquet tristique erat. Etiam laoreet mauris ut dapibus tincidunt. Pellentesque non ex at nisl ornare aliquam sed non ante. Nam lobortis magna id massa cursus, sit amet condimentum metus facilisis. Donec eu tortor at est tempor cursus et sed velit. Morbi rutrum elementum est vitae fringilla. Phasellus dignissim purus turpis, ac varius enim auctor vulputate. In ullamcorper vestibulum mauris. Nulla malesuada pretium mauris, lobortis eleifend dolor iaculis vitae.',
+  imageUrl: '/images/shoes-8.jpg',
+  averageRating: '5.0',
+}, {
+  id: '901',
+  name: 'Plain Shoes',
+  price: '54.00',
+  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel enim quam. Mauris nisl tellus, fringilla sed cursus eu, convallis non diam. Mauris quis fringilla nunc. Aenean leo lacus, lobortis sit amet venenatis a, aliquet tristique erat. Etiam laoreet mauris ut dapibus tincidunt. Pellentesque non ex at nisl ornare aliquam sed non ante. Nam lobortis magna id massa cursus, sit amet condimentum metus facilisis. Donec eu tortor at est tempor cursus et sed velit. Morbi rutrum elementum est vitae fringilla. Phasellus dignissim purus turpis, ac varius enim auctor vulputate. In ullamcorper vestibulum mauris. Nulla malesuada pretium mauris, lobortis eleifend dolor iaculis vitae.',
+  imageUrl: '/images/shoes-9.jpg',
+  averageRating: '5.0',
+},
+{
+  id: '112',
+  name: 'Teal Dress Shoes',
+  price: '330.00',
+  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel enim quam. Mauris nisl tellus, fringilla sed cursus eu, convallis non diam. Mauris quis fringilla nunc. Aenean leo lacus, lobortis sit amet venenatis a, aliquet tristique erat. Etiam laoreet mauris ut dapibus tincidunt. Pellentesque non ex at nisl ornare aliquam sed non ante. Nam lobortis magna id massa cursus, sit amet condimentum metus facilisis. Donec eu tortor at est tempor cursus et sed velit. Morbi rutrum elementum est vitae fringilla. Phasellus dignissim purus turpis, ac varius enim auctor vulputate. In ullamcorper vestibulum mauris. Nulla malesuada pretium mauris, lobortis eleifend dolor iaculis vitae.',
+  imageUrl: '/images/shoes-10.jpg',
+  averageRating: '5.0',
+},
+{
+  id: '223',
+  name: 'Fancy Boots',
+  price: '230.00',
+  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel enim quam. Mauris nisl tellus, fringilla sed cursus eu, convallis non diam. Mauris quis fringilla nunc. Aenean leo lacus, lobortis sit amet venenatis a, aliquet tristique erat. Etiam laoreet mauris ut dapibus tincidunt. Pellentesque non ex at nisl ornare aliquam sed non ante. Nam lobortis magna id massa cursus, sit amet condimentum metus facilisis. Donec eu tortor at est tempor cursus et sed velit. Morbi rutrum elementum est vitae fringilla. Phasellus dignissim purus turpis, ac varius enim auctor vulputate. In ullamcorper vestibulum mauris. Nulla malesuada pretium mauris, lobortis eleifend dolor iaculis vitae.',
+  imageUrl: '/images/shoes-11.jpg',
+  averageRating: '5.0',
+}, {
+  id: '334',
+  name: 'Gold Shoes',
+  price: '180.00',
+  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel enim quam. Mauris nisl tellus, fringilla sed cursus eu, convallis non diam. Mauris quis fringilla nunc. Aenean leo lacus, lobortis sit amet venenatis a, aliquet tristique erat. Etiam laoreet mauris ut dapibus tincidunt. Pellentesque non ex at nisl ornare aliquam sed non ante. Nam lobortis magna id massa cursus, sit amet condimentum metus facilisis. Donec eu tortor at est tempor cursus et sed velit. Morbi rutrum elementum est vitae fringilla. Phasellus dignissim purus turpis, ac varius enim auctor vulputate. In ullamcorper vestibulum mauris. Nulla malesuada pretium mauris, lobortis eleifend dolor iaculis vitae.',
+  imageUrl: '/images/shoes-12.jpg',
+  averageRating: '5.0',
+}];
+  
+  export let cartItems = [
+    products[0],
+    products[2],
+    products[3],
+  ];
+
+const app = express();
+app.use(bodyParser.json());
+
+app.get('/api/products', async (req, res) => {
+  const client = await MongoClient.connect(
+    'mongodb://localhost:27017',
+    { useNewUrlParser: true, useUnifiedTopology: true },
+  );
+  const db = client.db('vue-db');
+  const products = await db.collection('products').find({}).toArray();
+  res.status(200).json(products);
+  client.close();
+});
+
+app.get('/api/users/:userId/cart', async (req, res) => {
+  const { userId } = req.params;
+  const client = await MongoClient.connect(
+    'mongodb://localhost:27017',
+    { useNewUrlParser: true, useUnifiedTopology: true },
+  );
+  const db = client.db('vue-db');  
+  const user = await db.collection('users').findOne({ id: userId });
+  if (!user) return res.status(404).json('Could not find user!');
+  const products = await db.collection('products').find({}).toArray();
+  const cartItemIds = user.cartItems;
+  const cartItems = cartItemIds.map(id =>
+    products.find(product => product.id === id));
+  res.status(200).json(cartItems);
+  client.close();
+});
+
+app.get('/api/products/:productId', async (req, res) => {
+    const { productId } = req.params;
+    const client = await MongoClient.connect(
+      'mongodb://localhost:27017',
+      { useNewUrlParser: true, useUnifiedTopology: true },
+    );
+    const db = client.db('vue-db');
+    const product = await db.collection('products').findOne({ id: productId });
+    if (product) {
+        res.status(200).json(product);
+    } else {
+        res.status(404).json('Could not find the product!');
+    }
+    client.close();
+});
+
+app.post('/api/users/:userId/cart', async (req, res) => {
+  const { userId } = req.params;
+  const { productId } = req.body;
+  const client = await MongoClient.connect(
+    'mongodb://localhost:27017',
+    { useNewUrlParser: true, useUnifiedTopology: true },
+  );
+  const db = client.db('vue-db');
+  await db.collection('users').updateOne({ id: userId }, {
+    $addToSet: { cartItems: productId },
+  });
+  const user = await db.collection('users').findOne({ id: userId });
+  const cartItemIds = user.cartItems;
+  const cartItems = cartItemIds.map(id =>
+    products.find(product => product.id === id));
+  res.status(200).json(cartItems);
+  client.close();
+});
+
+app.delete('/api/users/:userId/cart/:productId', async (req, res) => {
+  const { userId, productId } = req.params;
+  const client = await MongoClient.connect(
+    'mongodb://localhost:27017',
+    { useNewUrlParser: true, useUnifiedTopology: true },
+  );
+  const db = client.db('vue-db');
+
+  await db.collection('users').updateOne({ id: userId }, {
+    $pull: { cartItems: productId },
+  });
+  const user = await db.collection('users').findOne({ id: userId });
+  const products = await db.collection('products').find({}).toArray();
+  const cartItemIds = user.cartItems;
+  const cartItems = cartItemIds.map(id =>
+    products.find(product => product.id === id));
+
+  res.status(200).json(cartItems);
+  client.close();
+});
+
+app.listen(8000, () => {
+    console.log('Server is listening on port 8000');
+});
---
+++ Exercise Files/CH04/04_04/End/fsv-front-end/src/views/ProductDetailPage.vue
@@ -0,0 +1,79 @@
+<template>
+  <div id="page-wrap" v-if="product">
+    <div id="img-wrap">
+      <img v-bind:src="product.imageUrl" />
+    </div>
+    <div id="product-details">
+      <h1>{{ product.name }}</h1>
+      <h3 id="price">${{ product.price }}</h3>
+      <p>Average rating: {{ product.averageRating }}</p>
+      <button
+        id="add-to-cart"
+        v-on:click="addToCart"
+      >Add to Cart</button>
+      <h4>Description</h4>
+      <p>{{ product.description }}</p>
+    </div>
+  </div>
+  <NotFoundPage v-else/>
+</template>
+
+<script>
+import axios from 'axios';
+import NotFoundPage from './NotFoundPage';
+
+export default {
+    name: 'ProductDetailPage',
+    components: {
+      NotFoundPage,
+    },
+    data() {
+      return {
+        product: {},
+      };
+    },
+    methods: {
+      async addToCart() {
+        await axios.post('/api/users/12345/cart', {
+          productId: this.$route.params.id,
+        });
+      },
+    },
+    async created() {
+      const result = await axios.get(`/api/products/${this.$route.params.id}`);
+      const product = result.data;
+      this.product = product;
+    }
+};
+</script>
+
+<style scoped>
+  #page-wrap {
+    margin-top: 16px;
+    padding: 16px;
+    max-width: 600px;
+  }
+
+  #img-wrap {
+    text-align: center;
+  }
+
+  img {
+    width: 400px;
+  }
+
+  #product-details {
+    padding: 16px;
+    position: relative;
+  }
+
+  #add-to-cart {
+    width: 100%;
+  }
+
+  #price {
+    position: absolute;
+    top: 24px;
+    right: 16px;
+  }
+</style>
